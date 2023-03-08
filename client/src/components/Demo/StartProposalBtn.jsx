879318ebcffd144047d440cd696f577bb83ba828
rem beuch <beucherie.r@gmail.com>
2023-03-08 14:52:14 +0100

    error require msg in startProposal

--- client/src/components/Demo/StartProposalBtn.jsx
+++ client/src/components/Demo/StartProposalBtn.jsx
@@ -1,13 +1,15 @@
+import { useState } from "react";
 import useEth from "../../contexts/EthContext/useEth";
 
 
 function StartProposalBtn() {
     const { state: { contract, accounts } } = useEth();
+    const [error, setError] = useState()
 
     const StartProposal = async e => {
         await contract.methods.startProposalsRegistering().call({ from: accounts[0] }, function (error, result) {
             if (error) {
-                console.log(error.message.split("revert ")[1]); // Affiche le message d'erreur du require()
+                setError(error.message.match(/revert (.*)/)[1]); // Affiche le message d'erreur du require()
             } else {
                 contract.methods.startProposalsRegistering().send({ from: accounts[0] }); // Affiche le résultat de la fonction si la condition est remplie
             }
@@ -20,7 +22,7 @@ function StartProposalBtn() {
             <div onClick={StartProposal} className="btn">
                 startProposalsRegistering
             </div>
-
+            {error}
         </div>
     );
 }
