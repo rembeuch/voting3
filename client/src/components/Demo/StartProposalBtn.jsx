53ebaf8c7d990012569619b48cccf8f6471f918c
rem beuch <beucherie.r@gmail.com>
2023-03-08 14:35:03 +0100

    add error

--- client/src/components/Demo/StartProposalBtn.jsx
+++ client/src/components/Demo/StartProposalBtn.jsx
@@ -5,9 +5,15 @@ function StartProposalBtn() {
     const { state: { contract, accounts } } = useEth();
 
     const StartProposal = async e => {
-        await contract.methods.startProposalsRegistering().send({ from: accounts[0] });
-    };
+        await contract.methods.startProposalsRegistering().call({ from: accounts[0] }, function (error, result) {
+            if (error) {
+                console.log(error.message.split("revert ")[1]); // Affiche le message d'erreur du require()
+            } else {
+                contract.methods.startProposalsRegistering().send({ from: accounts[0] }); // Affiche le résultat de la fonction si la condition est remplie
+            }
+        });
 
+    }
     return (
         <div className="btns">
 
@@ -18,5 +24,4 @@ function StartProposalBtn() {
         </div>
     );
 }
-
 export default StartProposalBtn;
